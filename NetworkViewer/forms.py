45a2f022f57e34a2db4fc8cdef36520d2b24958d
emmape <emma.e.persson@me.com>
2019-10-24 18:09:30 +0200

    adding possibility to input geneSpiderNetwork

--- NetworkViewer/forms.py
+++ NetworkViewer/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
+inputFormats= [
+    ('other', 'Other'),
+    ('geneSpider', 'GeneSpider')
+    ]
+
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
         label=''
     )
+    inputFormat= forms.CharField(label='Specify your input format', widget=forms.Select(choices=inputFormats))
--- NetworkViewer/views.py
+++ NetworkViewer/views.py
@@ -10,13 +10,9 @@ from .forms import DocumentForm
 from django.http import HttpResponseRedirect
 import csv
 from django.urls import resolve
-from django.utils.encoding import smart_str
 import matplotlib.pyplot as plt
 
-
-# Create your views here.
 def index(request):
-    # return render(request, 'NetworkViewer/index.html')
     if request.method == 'POST':
         upload = DocumentForm(request.POST, request.FILES)
         if upload.is_valid():
@@ -24,39 +20,57 @@ def index(request):
             with open('media/'+filename, 'wb+') as destination:
                 for chunk in request.FILES['docfile'].chunks():
                     destination.write(chunk)
-            return HttpResponseRedirect('/network/'+filename+"/")
+            fileType = upload.cleaned_data['inputFormat']
+            return HttpResponseRedirect('/network/'+filename+"/"+fileType+"/")
     else:
         upload = DocumentForm()
     return render(request,"NetworkViewer/index.html",{'form':upload})
 
-def network(request, n):
-
+def network(request, n, f):
+    fileType=f
     inputFile=n
     htmlFile = "./NetworkViewer/networks/"+n.split(".")[0]+".html"
-    inputFile=n
-    # inputFile="test.txt"
-    networkDf = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media/'+inputFile), sep=' ', header=None)
-    nxg = nx.from_pandas_edgelist(networkDf, source =0, target=1)
-    # nxg = nx.complete_graph(5)
+
+    ngx=None
+    if fileType=='geneSpider':
+        networkDf = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media/'+inputFile), sep=',', header=None)
+        nxg = nx.from_numpy_matrix(np.array(networkDf),create_using=nx.MultiDiGraph())
+    else:
+        networkDf = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media/'+inputFile), sep=' ', header=None)
+        nxg = nx.from_pandas_edgelist(networkDf, source =0, target=1)
+
+    #nxg = nx.complete_graph(5)
+    #nxg = nx.generators.directed.random_k_out_graph(10, 3, 0.5)
+
     diameter = 0#nx.diameter(nxg, e=None)
-    clusteringCoeffs = 0 #nx.clustering(nxg)
+    clusteringCoeffs = 0#nx.clustering(nxg)
     clusteringCoeff= 0#np.mean(list(clusteringCoeffs.values()))
     allCliques = None#nx.find_cliques(nxg)
-    cliques =0# len(list(allCliques))
+    cliques =0#len(list(allCliques))
+
     pos = nx.spring_layout(nxg)
     nx.draw(nxg, pos)
     plt.savefig("./NetworkViewer/templates/NetworkViewer/networks/"+inputFile.split(".")[0]+".png", format="PNG")
+    plt.clf()
 
     g = Network()
-    g.barnes_hut()
+    g.barnes_hut(gravity=-2000,
+        central_gravity=0.02,
+        spring_length=1,
+        spring_strength=0.000001,
+        damping=0.09,
+        overlap=0)
+
     g.toggle_physics(True)
     # g.show_buttons(filter_=['physics'])
+    if nx.is_directed(nxg):
+        g.directed=True
+
     g.from_nx(nxg)
     g.height="100%"
     g.width="100%"
-    # g.add_nodes([1,2,3], value=[10, 100, 400], title=["I am node 1", "node 2 here", "and im node 3"], x=[21.4, 54.2, 11.2], y=[100.2, 23.54, 32.1], label=["NODE 1", "NODE 2", "NODE 3"], color=["#00ff1e", "#162347", "#dd4b39"])
-    # G.from_nx(network)
     g.save_graph("./NetworkViewer/templates/NetworkViewer/networks/"+inputFile.split(".")[0]+".html")
+
     context={
         "diameter": diameter,
         "clustering": clusteringCoeff,
@@ -82,14 +96,14 @@ def downloadNetwork(request):
             return response
     raise Http404
 
-def downloadNetworkAsJpg(request):
+def downloadNetworkAsPng(request):
     file_path = "./NetworkViewer/templates/NetworkViewer/networks/"
     fileName=file_path+request.POST['file'].split(".")[0]+".html"
-    jpgFileName = file_path+request.POST['file'].split(".")[0]+".png"
+    pngFileName = file_path+request.POST['file'].split(".")[0]+".png"
 
-    if os.path.exists(jpgFileName):
-        with open(jpgFileName, 'rb') as fh:
+    if os.path.exists(pngFileName):
+        with open(pngFileName, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(jpgFileName)
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(pngFileName)
             return response
     raise Http404
